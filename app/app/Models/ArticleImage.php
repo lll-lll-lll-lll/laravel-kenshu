7041d9b9d3aab5ade13e67b617a657e31c10919d
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-18 15:08:34 +0900

    [feat] define article images model

---
+++ app/app/Models/ArticleImage.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ArticleImage extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'thumbnail_image_path',
+        'sub_image_path',
+        'created_at',
+        'article_id',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at ' => 'datetime',
+        ];
+    }
+}
