b91003a55d0f7c3f4582f40b7abc3f5e39ced75b
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-19 10:30:24 +0900

    [fix] ユーザの記事とタグを紐づける

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\ArticleImage;
+use App\Models\ArticleTag;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -18,8 +19,9 @@ class DatabaseSeeder extends Seeder
         User::factory(3)->create()->each(function ($user) {
             // 手動自動テストでパスワードがユーザごとに変わるのが面倒なので、passwrodで統一
             $user->password = 'password';
-            Article::factory(8)->create(['user_id' => $user->id])->each(function ($article) {
-                ArticleImage::factory(5)->create(['article_id' => $article->id]);});
+            Article::factory(5)->create(['user_id' => $user->id])->each(function ($article) {
+                ArticleTag::factory(rand(1,3))->create(['article_id' => $article->id, 'tag_id' => rand(1, 11)]);
+                ArticleImage::factory(rand(1,3))->create(['article_id' => $article->id]);});
         });
     }
 }
