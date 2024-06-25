1b87ba08efb1e6552a0bce53507f9b367b828642
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-25 10:21:08 +0900

    [fix]　 記事の紐付け

--- app/app/Models/ArticleImage.php
+++ app/app/Models/ArticleImage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ArticleImage extends Model
 {
@@ -38,5 +39,10 @@ class ArticleImage extends Model
         return [
             'created_at ' => 'datetime',
         ];
+    }
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
     }
 }
