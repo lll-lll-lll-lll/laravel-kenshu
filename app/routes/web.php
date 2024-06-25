2126784313bf73c91d0c65d226edd133860daaa6
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-25 15:36:40 +0900

    [feat]　記事削除Route

--- app/routes/web.php
+++ app/routes/web.php
@@ -23,6 +23,8 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
     Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
     Route::put('/articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
+    Route::delete('/articles/{article}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+
 });
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
