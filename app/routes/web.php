2acfbfe39a61d91663dc314757564c6df7af6430
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-25 15:09:54 +0900

    [feat]　記事更新　Route

--- app/routes/web.php
+++ app/routes/web.php
@@ -21,6 +21,8 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
     Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    Route::get('/articles/{article}/edit', [ArticleController::class, 'edit'])->name('articles.edit');
+    Route::put('/articles/{article}', [ArticleController::class, 'update'])->name('articles.update');
 });
 
 Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
