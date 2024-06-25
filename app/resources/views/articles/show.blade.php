457f6b800ba13c3c0c11dde2fc2fd90157bc9784
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-25 15:09:46 +0900

    [feat]　記事更新ページへの動線

--- app/resources/views/articles/show.blade.php
+++ app/resources/views/articles/show.blade.php
@@ -26,6 +26,11 @@
                 <span class="inline-block bg-gray-200 text-gray-800 text-xs px-2 py-1 rounded-full">{{ $tag->name }}</span>
             @endforeach
         </div>
+        <div class="mt-4 flex justify-end space-x-2">
+            <a href="{{ route('articles.edit', $article->id) }}" class="px-4 py-2 font-semibold text-white bg-blue-600 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+                Edit
+            </a>
+        </div>
     </div>
 </div>
 </body>
