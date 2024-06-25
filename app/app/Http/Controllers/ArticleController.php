30c920c6617f7edba1e407ac6b80fde470f28a64
shunpei.nakayama <shunpei.nakayama@prtimes.co.jp>
2024-06-25 10:50:16 +0900

    [feat]　 記事一覧取得 Controller

---
+++ app/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; // トレイトのインポート
+
+class ArticleController extends Controller
+{
+    use AuthorizesRequests; // トレイトの使用
+
+    public function index(): Factory|View|Application
+    {
+        $articles = Article::with('user', 'tags')->get();
+        return view('articles.index', compact('articles'));
+    }
+}
